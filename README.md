34a4806e3565d3aa68d03dd859589196ecc5a9bd
VaganovAlexanderMih <sashkavaganov3@gmail.com>
2023-04-21 05:04:46 +0300

    Modifying README.md

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# PythonProjects
-Python Projects by Vaganov Alexander, B05-220
+#Python encryption project.
+
+
+##About
+
+This [Encryption System](https://github.com/VaganovAlexanderMih/EncryptionSystem) can encrypt with the Caesar, Vigenere and Vernam ciphers,
+as well as decrypt them, and the Caesar cipher can be decrypted both with the use of a key (offset)
+and without it (using the method of frequency analysis)
+When you use Vigenere and Vernam ciphers, the key is generating automatically
+and randomly. You can find it in path that you left for the "key" option.
+
+
+##Installation
+
+1. To run, go to the command line and type 
+2. "git clone --branch dev git@github.com:VaganovAlexanderMih/PythonProjects.git PythonProjects"
+3. "cd PythonProjects"
+4. "cd EncryptionSystem"
+5. "pip install -r requirements.txt".
+6. Then type "python3 main.py --help" to see all use cases.
+
+
+###How to use the program:
+
+- Write "--source" or "-s" to set source to the file that needed to be coded/decoded
+- Write "--cipher" or "-c" to set cipher/decoder (Caesar to set the "Caesar cipher" then offset, Vernam to set "Vernam cipher", Vigenere to set "Viegenere cipher")
+- Write "--output" or "-o" to set output file
+- Write "--decode" or "-d" to decode file (write nothing if you want to code it)
+
+![Printing help](src/images/printing_help.png)
+
+
+##Examples of using
+
+Let's see how to work with Vernam cipher:
+
+Source file:
+![Source](src/images/source.png)
+
+Command sequence to launch the program:
+![Vernam cipher command](src/images/vernam_cipher_command.png)
+
+Key file:
+![Key](src/images/key.png)
+
+Output file:
+![Output](src/images/output.png)
---
+++ main.py
@@ -0,0 +1,104 @@
+import sys
+import os
+cur_path = os.getcwd()
+cur_path += "/src"
+sys.path.insert(1, cur_path)
+import Cryptographer
+import Decoder
+
+source = ""
+cipher = ""
+offset = 0
+output = ""
+decoder = False
+skip_to = 0
+
+# Parsing command line arguments
+if __name__ == "__main__":
+    for i in range (len(sys.argv)):
+        if (skip_to >= i):
+            continue
+        if (sys.argv[i] == "--help"):
+            print("""Write "--source" or "-s" to set source to the file that needed to be coded/decoded
+Write "--cipher" or "-c" to set cipher/decoder (Caesar to set the "Caesar cipher" then offset (0 is default value), Vernam to set "Vernam cipher", Vigenere to set "Viegenere cipher")
+Write "--output" or "-o" to set output file
+Write "--decode" or "-d" to decode file (write nothing if you want to code it) """)
+        elif (sys.argv[i] == "--source" or sys.argv[i] == "-s"):
+            source = sys.argv[i + 1]
+            skip_to = i + 1
+        elif (sys.argv[i] == "--cipher" or sys.argv[i] == "-c"):
+            cipher = sys.argv[i + 1]
+            skip_to = i + 1
+        elif (sys.argv[i] == "--output" or sys.argv[i] == "-o"):
+            output = sys.argv[i + 1]
+            skip_to = i + 1
+        elif (sys.argv[i] == "--decoder" or sys.argv[i] == "-d"):
+            decoder = True
+        else:
+            print("Invalid argument. Write --help to get args list")
+
+if (source == "" or cipher == "" or output == ""):
+    print("Not enough arguments, please write --help to get args list")
+    sys.exit()
+
+# Rewrite output file?
+rewrite_output_file = ""
+print("Do you want to rewrite the output file?")
+rewrite_output_file = str(input())
+if (rewrite_output_file == "y"):
+    f = open(output, "w")
+    f.write('')
+    f.close()
+elif (rewrite_output_file != "y" and rewrite_output_file != "n"):
+    print("Invalid argument. Aborting.")
+    sys.exit()
+
+# Crypting/Decoding input text
+if (decoder):
+    if (cipher == "Vernam"):
+        print("Insert the KeyPath, please")
+        key_path = str(input())
+        Decoder.DecoderVernamCipher(source, key_path, output)
+    elif (cipher == "Vigenere"):
+        print("Insert the KeyPath, please")
+        key_path = str(input())
+        Decoder.DecoderVigenereCipher(source, key_path, output)
+    elif (cipher == "Caesar"):
+        print("Do you have the OffsetKey? [y/n]")
+        decoder_version = str(input())
+        if (decoder_version == "y"):
+            print("Then print it)))")
+            offset = int(input())
+            Decoder.DecoderCaesarCipherWithKey(source, offset, output)
+        elif (decoder_version == "n"):
+            Decoder.DecoderCaesarCipherWithoutKey(source, output)
+        else:
+            print("Invalid argument. Aborting")
+            sys.exit()
+else:
+    if (cipher == "Vernam"):
+        print("Insert KeyPath, please")
+        key_path = str(input())
+        f = open(key_path, "w")
+        f.write('')
+        f.close()
+        with open(source) as inp:
+            for string in inp:
+                Cryptographer.VernamCipher(string, key_path, output)
+    elif (cipher == "Vigenere"):
+        print("Insert KeyPath, please")
+        key_path = str(input())
+        f = open(key_path, "w")
+        f.write('')
+        f.close()
+        with open(source) as inp:
+            for string in inp:
+                Cryptographer.VigenereCipher(string, key_path, output)
+    elif (cipher == "Caesar"):
+        print("Input the offset, please")
+        offset = int(input())
+        with open(source) as inp:
+            for string in inp:
+                Cryptographer.CaesarCipher(string, offset, output)
+
+print("Done!")
